2380c28c87531ccc5040ab709171c9d595b6240a
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-30 21:54:59 +0530

    [sanjana] Add : Student Record

---
+++ Lambda_Expression/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_Expression
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+    }
+}
